d7145ac8b319babc0576c3d7ae0eeca267648338
Alexandre Gariépy <alexandre.gariepy@rakuten.com>
2019-08-29 16:28:04 -0400

    Add auto-detangle mode

--- literate-clojure-mode.el
+++ literate-clojure-mode.el
@@ -1,4 +1,4 @@
-
+;; -*- lexical-binding: t -*-
 ;;; literate-clojure-mode.el --- Tools for literate clojure in org-mode
 
 ;; Copyright (C) 2019 Rakuten Ready
@@ -18,12 +18,18 @@
 
 ;; Variables
 
-(defvar litclj-auto-follow-mode t)
+(defcustom litclj-auto-detangle-delay "500 millisec"
+  "Time delay after which a modified code block is detangled")
 
-(defvar litclj-follow-last-file-id nil)
+(defvar litclj-follow-last-block-infos nil)
 (defvar litclj-follow-file-blocks-cache '())
+(defvar litclj-detangle-timers '())
 
 ;; Helper functions
+
+(defun litclj--strip-text-properties(txt)
+  (set-text-properties 0 (length txt) nil txt)
+  txt)
 
 (defun litclj--get-block-tangle-property ()
   (cdr
@@ -118,21 +124,34 @@
         (litclj--next-non-empty-line)
         (- point-pos (point))))))
 
-(defun litclj--org-file-and-id ()
-    (save-excursion
-      (re-search-backward org-bracket-link-analytic-regexp nil t)
-      (let* ((full-path (match-string 3))
-             (block-name (match-string 5))
-             (path (progn (string-match "::" full-path)
+(defun litclj--code-block-infos ()
+  "Returns the origin file of a code block, its name and its point in the tangled file."
+  (save-excursion
+    (let* ((point (re-search-backward org-bracket-link-analytic-regexp nil t))
+           (full-path (match-string 3))
+           (block-name (match-string 5))
+           (path (progn (string-match "::" full-path)
                         (substring full-path 0 (match-beginning 0)))))
-        `(,path ,block-name))))
+      `(,point ,path ,block-name))))
+
+(defun litclj--search-block-end (block-name)
+  (re-search-forward (concat " " (regexp-quote block-name)
+                             " ends here") nil t))
 
 (defun litclj--in-tangled-block? ()
   (save-excursion
-    (-let [(_ block-name) (litclj--org-file-and-id)]
-      (re-search-forward (concat " " (regexp-quote block-name)
-                                 " ends here")
-                         nil t))))
+    (-let [(_ _ block-name) (litclj--code-block-infos)]
+
+      (litclj--search-block-end block-name))))
+
+(defun litclj--tangled-block-content ()
+  (-let* (((begin-header-point _ block-name) (litclj--code-block-infos))
+          (end-footer-point (save-excursion (litclj--search-block-end block-name))))
+    (buffer-substring-no-properties
+     (save-excursion (goto-char begin-header-point)
+                     (line-beginning-position 2))
+     (save-excursion (goto-char end-footer-point)
+                     (line-end-position 0)))))
 
 (defun litclj--goto-point-subheading (point)
   "Show the content of the subheading at point."
@@ -153,10 +172,11 @@
       (beginning-of-line)
       (forward-line)
       (forward-char point-pos))))
+
 (defun litclj-tangle-goto-org ()
   (interactive)
   (if (litclj--in-tangled-block?)
-      (-let [(path block-name) (litclj--org-file-and-id)]
+      (-let [(_ path block-name) (litclj--code-block-infos)]
         (find-file-other-window path)
         (let ((block-point (cdr (assoc block-name (litclj--block-name-to-point-assoc-list)))))
           (litclj--goto-point-subheading block-point)
@@ -165,16 +185,45 @@
           (scroll-up-line)))
     (error "Not in tangled code")))
 
+(defun litclj-detangle-current-block ()
+  (interactive)
+  (let ((b (current-buffer))
+        (content (litclj--tangled-block-content)))
+    (litclj-tangle-goto-org)
+    (org-babel-update-block-body content)
+    (switch-to-buffer-other-window b)))
+
 (defun litclj-follow ()
   (interactive)
   (ignore-errors
-    (let ((file-id (litclj--org-file-and-id)))
-      (unless (and (string= (nth 0 file-id) (nth 0 litclj-follow-last-file-id))
-                   (string= (nth 1 file-id) (nth 1 litclj-follow-last-file-id)))
+    (let ((block-infos (litclj--code-block-infos)))
+      (unless (string= (nth 2 block-infos)
+                       (nth 2 litclj-follow-last-block-infos))
         (let ((b (current-buffer)))
           (litclj-tangle-goto-org)
           (switch-to-buffer-other-window b)
-          (setq litclj-follow-last-file-id file-id))))))
+          (setq litclj-follow-last-block-infos block-infos))))))
+
+(defun litclj--auto-detangle-current-block (begin end lenght)
+  (-let [(_ file name) (litclj--code-block-infos)]
+    (when-let (timer (assoc (litclj--strip-text-properties name) litclj-detangle-timers))
+      (cancel-timer (cdr timer))
+      (setq litclj-detangle-timers (delq timer litclj-detangle-timers)))
+    (let ((current-point (point))
+          (b (current-buffer)))
+      (setq litclj-detangle-timers
+            (cons `(,(litclj--strip-text-properties name) .
+                    ,(run-at-time litclj-auto-detangle-delay
+                                  nil
+                                  (lambda ()
+                                    (with-current-buffer b
+                                      (save-excursion
+                                        (goto-char current-point)
+                                        (litclj-detangle-current-block))))))
+                  litclj-detangle-timers)))))
+
+(defun litclj--invalid-cache-on-tangle ()
+  (setq litclj-follow-file-blocks-cache '()))
 
 ;;;###autoload
 (define-minor-mode literate-clojure-mode
@@ -185,10 +234,21 @@
 (define-minor-mode literate-clojure-follow-mode
   "Automatically follow org mode files from tangles clojure files."
   :init nil
-  (if (not literate-clojure-mode)
+  (if literate-clojure-follow-mode
       (progn
-        (add-hook 'post-command-hook 'litclj-follow nil t))
-    (remove-hook 'post-command-hook 'litclj-follow t)))
+        (add-hook 'post-command-hook 'litclj-follow nil t)
+        (add-hook 'org-babel-post-tangle-hook 'litclj--invalid-cache-on-tangle))
+    (progn
+      (remove-hook 'post-command-hook 'litclj-follow t)
+      (remove-hook 'org-babel-post-tangle-hook 'litclj--invalid-cache-on-tangle))))
+
+(define-minor-mode literate-clojure-auto-detangle-mode
+  "Automatically detangles clojure files on modification"
+  :init nil
+  (if literate-clojure-auto-detangle-mode
+      (add-hook 'after-change-functions 'litclj--auto-detangle-current-block nil t)
+    (remove-hook 'after-change-functions 'litclj--auto-detangle-current-block t)))
+
 
 (provide 'literate-clojure-mode)
 ;;; literate-clojure-mode.el ends here
